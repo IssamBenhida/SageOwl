02afa8e4d9481c92962694e0aecba434604fca2c
IssamxBen <issam.benhida.911@gmail.com>
2024-08-05 21:23:48 +0000

    v0.6

--- main.tf
+++ main.tf
@@ -7,34 +7,34 @@ terraform {
   }
 }
 
-resource "aws_elasticsearch_domain" "es-local" {
+resource "aws_cloudwatch_log_group" "example" {
+  name = "localstack-log-group"
+}
+
+resource "aws_cloudwatch_log_stream" "example" {
+  log_group_name = aws_cloudwatch_log_group.example.name
+  name           = "local-instance"
+  depends_on     = [aws_cloudwatch_log_group.example]
+}
+
+resource "aws_elasticsearch_domain" "es_local" {
   domain_name           = "es-local"
   elasticsearch_version = "7.10"
+  depends_on            = [aws_cloudwatch_log_stream.example]
 }
 
 resource "aws_s3_bucket" "backup" {
-  bucket = "kinesis-activity-backup-local"
-}
-
-
-resource "aws_kinesis_stream" "test_stream" {
-  name        = "kinesis-es-local-stream"
-  shard_count = 2
+  bucket     = "kinesis-activity-backup-local"
+  depends_on = [aws_elasticsearch_domain.es_local]
 }
 
 resource "aws_kinesis_firehose_delivery_stream" "example" {
   name        = "activity-to-elasticsearch-local"
   destination = "elasticsearch"
 
-
-  kinesis_source_configuration {
-    kinesis_stream_arn = "arn:aws:kinesis:us-east-1:000000000000:stream/kinesis-es-local-stream"
-    role_arn           = "arn:aws:iam::000000000000:role/Firehose-Reader-Role"
-  }
-
   elasticsearch_configuration {
     role_arn       = "arn:aws:iam::000000000000:role/Firehose-Reader-Role"
-    domain_arn     = "arn:aws:es:us-east-1:000000000000:domain/es-local"
+    domain_arn     = aws_elasticsearch_domain.es_local.arn
     index_name     = "activity"
     type_name      = "activity"
     s3_backup_mode = "AllDocuments"
@@ -43,9 +43,32 @@ resource "aws_kinesis_firehose_delivery_stream" "example" {
       role_arn   = "arn:aws:iam::000000000000:role/Firehose-Reader-Role"
       bucket_arn = "arn:aws:s3:::kinesis-activity-backup-local"
     }
-  }
 
-  depends_on = [aws_elasticsearch_domain.es-local, aws_kinesis_stream.test_stream, aws_s3_bucket.backup]
+    processing_configuration {
+      enabled = true
+      processors {
+        type = "Lambda"
+        parameters {
+          parameter_name  = "LambdaArn"
+          parameter_value = "arn:aws:lambda:us-east-1:000000000000:function:mylambda"
+        }
+        parameters {
+          parameter_name  = "RoleArn"
+          parameter_value = "arn:aws:iam::000000000000:role/LambdaRole"
+        }
+      }
+    }
+  }
+  depends_on = [aws_s3_bucket.backup]
+}
+resource "aws_cloudwatch_log_subscription_filter" "example" {
+  log_group_name  = aws_cloudwatch_log_group.example.name
+  name            = "kinesis_test"
+  filter_pattern  = ""
+  destination_arn = aws_kinesis_firehose_delivery_stream.example.arn
+  role_arn        = "arn:aws:iam::000000000000:role/kinesis_role"
+
+  depends_on = [aws_kinesis_firehose_delivery_stream.example]
 }
 
 output "DeliveryStreamARN" {
